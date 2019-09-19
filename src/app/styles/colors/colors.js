5017b6e797a9b17ea6ae7238b4dc5ae100410e56
David de Lusenet <david@lusenet.nl>
2019-09-19 12:08:09 +0200

    Add colors.js

---
+++ src/app/styles/colors/colors.js
@@ -0,0 +1,29 @@
+export const blue25 = '#BFE7FE';
+
+export const blue50 = '#80CFFE';
+
+export const blue100 = '#009FFD';
+
+export const purple50 = '#9BA1C9';
+
+export const purple100 = '#3D4A9A';
+
+export const green = '#2DD67B';
+
+export const orange = '#EB6500';
+
+export const red = '#F94E4E';
+
+export const sl2 = '#FAFBFC';
+
+export const sl5 = '#F2F4F6';
+
+export const sl10 = '#E5E9ED';
+
+export const sl25 = '#BFC8D3';
+
+export const sl50 = '#7F91A8';
+
+export const sl75 = '#3F5A7C';
+
+export const sl100 = '#002451';
--- src/app/styles/theme/theme.js
+++ src/app/styles/theme/theme.js
@@ -1,21 +1,6 @@
-/* eslint-disable no-unused-vars, sort-keys */
-// The sort keys ESLint rule is disabled in this file so we can group our variables
-// Color names by http://chir.ag/projects/name-that-color/
-const blue25 = '#BFE7FE';
-const blue50 = '#80CFFE';
-const blue100 = '#009FFD';
-const purple50 = '#9BA1C9';
-const purple100 = '#3D4A9A';
-const green = '#2DD67B';
-const orange = '#EB6500';
-const red = '#F94E4E';
-const sl2 = '#FAFBFC';
-const sl5 = '#F2F4F6';
-const sl10 = '#E5E9ED';
-const sl25 = '#BFC8D3';
-const sl50 = '#7F91A8';
-const sl75 = '#3F5A7C';
-const sl100 = '#002451';
+/* eslint-disable sort-keys */
+// The sort keys ESLint rule is disabled in this file so we can group our variables in any way we like
+import { purple100, sl10 } from '../colors/colors';
 
 const theme = {
     buttonPrimaryColor: purple100,
