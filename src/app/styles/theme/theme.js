296c479db5ad3698168fdcc4de9d20161df9e64a
David de Lusenet <david@lusenet.nl>
2019-09-19 11:19:01 +0200

    Add colors to theme

--- src/app/styles/theme/theme.js
+++ src/app/styles/theme/theme.js
@@ -1,12 +1,25 @@
-/* eslint-disable sort-keys */
+/* eslint-disable no-unused-vars, sort-keys */
 // The sort keys ESLint rule is disabled in this file so we can group our variables
 // Color names by http://chir.ag/projects/name-that-color/
-const athensGray = '#E5E9ED';
-const chambray = '#3D4A9A';
+const blue25 = '#BFE7FE';
+const blue50 = '#80CFFE';
+const blue100 = '#009FFD';
+const purple50 = '#9BA1C9';
+const purple100 = '#3D4A9A';
+const green = '#2DD67B';
+const orange = '#EB6500';
+const red = '#F94E4E';
+const sl2 = '#FAFBFC';
+const sl5 = '#F2F4F6';
+const sl10 = '#E5E9ED';
+const sl25 = '#BFC8D3';
+const sl50 = '#7F91A8';
+const sl75 = '#3F5A7C';
+const sl100 = '#002451';
 
 const theme = {
-    buttonPrimaryColor: chambray,
-    buttonDisabledColor: athensGray,
+    buttonPrimaryColor: purple100,
+    buttonDisabledColor: sl10,
     buttonTextColor: 'white',
     buttonSmallBorderRadius: '16px',
     buttonSmallHeight: '32px',
