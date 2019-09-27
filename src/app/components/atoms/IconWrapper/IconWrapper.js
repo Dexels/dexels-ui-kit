2b955244230a8af644a08cec2822381ce8cd2567
David de Lusenet <david@lusenet.nl>
2019-09-27 11:41:13 +0200

    Remove IconWrapper and TextWrapper components

--- src/app/components/atoms/IconWrapper/IconWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const IconWrapper = styled.div`
-    flex: 0 0 auto;
-    order: 1;
-    margin: 0 6px 0 0;
-`;
-
-export default IconWrapper;
--- src/app/components/atoms/TextWrapper/TextWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const TextWrapper = styled.p`
-    flex: 0 0 auto;
-    order: 2;
-`;
-
-export default TextWrapper;
