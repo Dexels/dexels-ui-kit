610e2d500406bb296af15f972c579263fd638888
David de Lusenet <david@lusenet.nl>
2019-09-30 11:43:19 +0200

    Add toLowerCase param to mapArrayToObject func

--- src/app/utils/mapArrayToObject.js
+++ src/app/utils/mapArrayToObject.js
@@ -1,9 +1,11 @@
-export default (array) => {
+const mapArrayToObject = (array, toLowerCase = false) => {
     const object = {};
 
     array.forEach((entry) => {
-        object[entry] = entry;
+        object[entry] = toLowerCase ? entry.toLowerCase() : entry;
     });
 
     return object;
 };
+
+export default mapArrayToObject;
