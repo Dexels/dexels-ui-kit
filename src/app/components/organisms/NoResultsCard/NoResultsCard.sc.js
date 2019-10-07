5279d58538d68aa4c5ef8e3ceabb5b86fc1103b7
Erik Versteeg <erik@dexels.com>
2019-10-07 08:51:25 +0200

    Changed margin on title part

--- src/app/components/organisms/NoResultsCard/NoResultsCard.sc.js
+++ src/app/components/organisms/NoResultsCard/NoResultsCard.sc.js
@@ -25,7 +25,7 @@ Header.defaultProps = {
 export const Title = styled.div`
     ${({ theme }) => theme.textStyling(theme.availableTextStyles().body1)};
     font-family: ${({ theme }) => theme.noResultsCard.fontFamilyTitle};
-    margin: 4px;
+    margin: 4px 4px 4px 0px;
     color: ${({ theme }) => theme.noResultsCard.colorTitle};
 `;
 
