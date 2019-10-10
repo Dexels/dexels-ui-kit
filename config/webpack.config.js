6a5cbeb69be4939abb351d1abaaf147389975287
David de Lusenet <david@lusenet.nl>
2019-10-10 15:50:50 +0200

    Replace webpack files by webpack.config.js

---
+++ config/webpack.config.js
@@ -0,0 +1,86 @@
+const { distPath, libPath, publicPath } = require('./paths');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { resolve } = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: resolve(__dirname, `${libPath}/index.js`),
+    externals: {
+        react: {
+            amd: 'react',
+            commonjs: 'react',
+            commonjs2: 'react',
+            root: 'React',
+            umd: 'react',
+        },
+        'react-dom': {
+            amd: 'react-dom',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            root: 'ReactDOM',
+            umd: 'react-dom',
+        },
+        'styled-components': {
+            amd: 'styled-components',
+            commonjs: 'styled-components',
+            commonjs2: 'styled-components',
+            root: 'styled',
+            umd: 'styled-components',
+        },
+    },
+    module: {
+        rules: [
+            {
+                exclude: /(node_modules)/,
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts',
+                            publicPath: 'fonts',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    output: {
+        filename: 'index.js',
+        library: 'DexelsUIKit',
+        libraryTarget: 'umd',
+        path: distPath,
+        publicPath: '/',
+    },
+    plugins: [
+        new BundleAnalyzerPlugin(),
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        new MiniCSSExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    resolve: {
+        alias: {
+            fonts: resolve(__dirname, `${publicPath}/fonts`),
+        },
+    },
+};
