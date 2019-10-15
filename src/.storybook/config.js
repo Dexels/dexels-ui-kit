bba39135fd89aee5494abe17bba6d6bf9026a435
David de Lusenet <david@lusenet.nl>
2019-10-15 15:25:13 +0200

    Add storybook-addon-styled-component-theme configuration

--- src/.storybook/config.js
+++ src/.storybook/config.js
@@ -2,33 +2,44 @@ import '../app/styles/fonts/exo2/exo2.css';
 import '../app/styles/fonts/iconfont/iconfont.css';
 import '../app/styles/fonts/opensans/opensans.css';
 import { addDecorator, configure } from '@storybook/react';
-import { MyThemeProvider } from '../app/styles/theme/ThemeContext';
 import React from 'react';
 // This seems like a ESLint bug
 // eslint-disable-next-line import/no-unresolved
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
-
-const theme = {
-    layout: 'basic',
-    mode: 'basic',
-};
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
 // Make info and knobs addons available in all stories
 addDecorator(withInfo);
 addDecorator(withKnobs);
 
+addDecorator(withThemesProvider([
+    {
+        layout: 'basic',
+        mode: 'basic',
+        name: 'basic',
+    },
+    {
+        layout: 'basic',
+        mode: 'dark',
+        name: 'dark',
+    },
+    {
+        layout: 'basic',
+        mode: 'light',
+        name: 'light',
+    },
+]));
+
 // Wrap all stories in the ThemeProvider and render the BaseStyling
 addDecorator((storyFn) => (
-    <MyThemeProvider theme={theme}>
-        <div style={{
-            margin: '40px auto',
-            width: '80%',
-        }}
-        >
-            {storyFn()}
-        </div>
-    </MyThemeProvider>
+    <div style={{
+        margin: '40px auto',
+        width: '80%',
+    }}
+    >
+        {storyFn()}
+    </div>
 ));
 
 // Import all stories
