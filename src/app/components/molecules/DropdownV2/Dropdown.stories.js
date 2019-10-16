9a8045af6a9fa9191210338e6624e5ef6c834302
David de Lusenet <david@lusenet.nl>
2019-10-16 15:53:24 +0200

    Rename unused defaultValue prop + rename to value/setValue

--- src/app/components/molecules/DropdownV2/Dropdown.stories.js
+++ src/app/components/molecules/DropdownV2/Dropdown.stories.js
@@ -7,7 +7,7 @@ export default { title: 'molecules/DropdownV2' };
 export const Configurable = () => {
     const placeholder = 'Select your fruit';
     const fruits = ['Banana', 'Apple', 'Orange', 'Pear', 'Strawberry'];
-    const [selectedValue, setSelectedValue] = useState(placeholder);
+    const [value, setValue] = useState(placeholder);
 
     const optionArray = fruits.map((fruit, index) => (
         <option disabled={index === 2} key={fruit} value={fruit}>
@@ -21,22 +21,21 @@ export const Configurable = () => {
                 {'What is the best fruit?'}
             </p>
             <Dropdown
-                defaultValue="placeholder-value"
                 errorMessage={text('Error message', 'Everything is broken, oops')}
                 hasError={boolean('Has error', Dropdown.defaultProps.hasError)}
                 isDisabled={boolean('Is disabled', Dropdown.defaultProps.isDisabled)}
                 isValid={boolean('Is valid', Dropdown.defaultProps.isValid)}
                 name="the-best-fruit"
                 onChange={(event) => {
-                    setSelectedValue(event.currentTarget.value);
+                    setValue(event.currentTarget.value);
                 }}
                 placeholder={placeholder}
-                value={selectedValue}
+                value={value}
             >
                 {optionArray}
             </Dropdown>
             <p>
-                {`You selected ${placeholder === selectedValue ? 'nothing yet' : selectedValue}.`}
+                {`You selected ${placeholder === value ? 'nothing yet' : value}.`}
             </p>
         </>
     );
