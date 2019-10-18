50cc2e38f2046bc0f8bfd2f1200a24bb600155c1
Erik Versteeg <erik@dexels.com>
2019-10-18 10:35:26 +0200

    Themed Tooltip and TextIcon

--- src/app/styles/theme/layout.js
+++ src/app/styles/theme/layout.js
@@ -10,12 +10,17 @@ export const getAvailableThemeLayouts = () => (
     Object.values(themeLayouts)
 );
 
-export const spacingUnit = theme('layout', {
-    [themeLayouts.basic]: '8px',
-    [themeLayouts.compact]: '4px',
-});
-
 export const borderRadius = theme('layout', {
     [themeLayouts.basic]: '8px',
     [themeLayouts.compact]: '6px',
+});
+
+export const defaultIconSize = theme('layout', {
+    [themeLayouts.basic]: '24px',
+    [themeLayouts.compact]: '22px',
+});
+
+export const spacingUnit = theme('layout', {
+    [themeLayouts.basic]: '8px',
+    [themeLayouts.compact]: '4px',
 });
