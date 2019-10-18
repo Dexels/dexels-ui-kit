5ee43eaf2bbe49dde3eb0eecc6619b44c2065ac6
Maria Papadaki <mpapadaki@dexels.com>
2019-10-18 12:46:01 +0200

    Did some css changes and reordering

--- src/app/components/organisms/Header/Header.sc.js
+++ src/app/components/organisms/Header/Header.sc.js
@@ -6,46 +6,48 @@ import {
 import { ELEVATIONS } from '../../../utils/constants';
 import getElevation from '../../../styles/mixins/getElevation';
 import PropTypes from 'prop-types';
+import { spacingUnit } from '../../../styles/theme/layout';
 import styled from 'styled-components';
-
-export const ButtonContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-self: center;
-    margin-right: 16px;
-`;
-
-export const Title = styled.div`
-    ${textStyling(availableTextStyles().body1)};
-    margin: auto;
-    padding-left: 16px;
-    color: ${colorHeadingLight};
-`;
-
-export const FunctionalWrapper = styled.div`
-    display: flex;
-    flex-wrap: nowrap;
-    align-self: center;
-    color: ${colorHeadingLight};
-    margin-inline-start: auto;
-`;
-
-export const NavigationWrapper = styled.div`
-    display: flex;
-    flex-wrap: nowrap;
-    align-self: center;
-    color: ${colorHeadingLight};
-`;
 
 export const StyledHeader = styled.div`
     ${({ elevation }) => getElevation(elevation)};
     display: flex;
     flex-direction: row;
+    align-items: center;
     background: ${backgroundColorTertiary};
-    height: 52px;
+    min-height: calc(${spacingUnit} * 6);
     color: ${colorHeadingLight};
 `;
 
 StyledHeader.propTypes = {
     elevation: PropTypes.oneOf(Object.values(ELEVATIONS)).isRequired,
 };
+
+export const NavigationWrapper = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    /* align-self: center; */
+    color: ${colorHeadingLight};
+`;
+
+export const Title = styled.div`
+    ${textStyling(availableTextStyles().body1)};
+    margin: 0 16px 0 0;
+    color: ${colorHeadingLight};
+    /* align-self: center; */
+`;
+
+export const FunctionalWrapper = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    /* align-self: center; */
+    color: ${colorHeadingLight};
+    margin-inline-start: auto;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    /* align-self: center; */
+    margin: 0 16px 0 0;
+`;
