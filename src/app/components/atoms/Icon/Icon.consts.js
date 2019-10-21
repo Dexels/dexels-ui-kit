3e9278e856595f9e65363236d8cff7ec3d1ea44a
David de Lusenet <david@lusenet.nl>
2019-10-21 09:01:41 +0200

    Remove props from Icon atom

--- src/app/components/atoms/Icon/Icon.consts.js
+++ src/app/components/atoms/Icon/Icon.consts.js
@@ -1,6 +1,4 @@
-import * as colors from '../../../styles/colors/colors';
-
-// Since we don't want to include a selection.json in our distribution bundle we've come up with the following solution:
+// Since we don't want to include a selection.json in our distribution bundle we've came up with the following solution:
 // - If the contents of selection.json ever change just uncomment the import below and start Storybook
 // - Open the console, the generated icon types should be logged there
 // - Copy these to this file and outcomment the import again
@@ -9,9 +7,6 @@ import * as colors from '../../../styles/colors/colors';
 // I know this solution is a little bit weird but it's still better than including a 40 kb JSON file in our bundle
 // If you manage to come up with a better solution don't hesitate to implement it!
 // import '../../../utils/getIconTypes';
-
-export const ICON_COLORS = colors;
-
 export const ICON_TYPES = {
     ADD: 'Add',
     ALERT: 'Alert',
@@ -77,3 +72,5 @@ export const ICON_TYPES = {
     VISIBILITY_ON: 'Visibility-On',
     WORLD: 'World',
 };
+
+export default ICON_TYPES;
--- src/app/components/atoms/Icon/Icon.js
+++ src/app/components/atoms/Icon/Icon.js
@@ -1,29 +1,15 @@
-import { ICON_COLORS, ICON_TYPES } from './Icon.consts';
-import { defaultIconSize } from '../../../styles/theme/layout';
+import { ICON_TYPES } from './Icon.consts';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyledIcon } from './Icon.sc';
 
-const Icon = ({ color, size, type }) => (
-    <StyledIcon
-        className={`icon-${type}`}
-        color={color}
-        size={size}
-    />
+const Icon = ({ type }) => (
+    <span className={`icon-${type}`} />
 );
 
-Icon.colors = ICON_COLORS;
 Icon.types = ICON_TYPES;
 
 Icon.propTypes = {
-    color: PropTypes.oneOf(Object.values(Icon.colors)),
-    size: PropTypes.string,
     type: PropTypes.oneOf(Object.values(Icon.types)).isRequired,
-};
-
-Icon.defaultProps = {
-    color: null,
-    size: defaultIconSize,
 };
 
 export default Icon;
