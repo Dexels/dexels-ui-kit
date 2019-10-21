ebcd3cae0fc5bd7d21630e9862149c7571b2143c
David de Lusenet <david@lusenet.nl>
2019-10-21 09:03:32 +0200

    Add configurable wrapper around Icon in stories

--- src/app/components/atoms/Icon/Icon.stories.js
+++ src/app/components/atoms/Icon/Icon.stories.js
@@ -1,13 +1,27 @@
-import { select, text } from '@storybook/addon-knobs';
+import * as colors from '../../../styles/colors/colors';
 import Icon from './Icon';
 import React from 'react';
+import { select } from '@storybook/addon-knobs';
 
 export default { title: 'atoms/Icon' };
 
+/* eslint-disable sort-keys */
+// The sort-keys ESLint rule is disabled so that we can order the sizes from small to large
 export const Configurable = () => (
-    <Icon
-        color={select('Icon color', Icon.colors, Icon.colors.grey100)}
-        size={text('Size in px', '24px')}
-        type={select('Type', Icon.types, Icon.types.ARROW_DOWN)}
-    />
+    <div
+        style={{
+            color: select('Color', colors, colors.blue100),
+            fontSize: select('Size', {
+                XS: '8px',
+                S: '12px',
+                M: '16px',
+                L: '20px',
+                XL: '24px',
+                XXL: '28px',
+            }, '16px'),
+        }}
+    >
+        <Icon type={select('Type', Icon.types, Icon.types.ARROW_DOWN)} />
+    </div>
 );
+/* eslint-enable */
