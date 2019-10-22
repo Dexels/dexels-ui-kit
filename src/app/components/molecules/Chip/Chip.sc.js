58006074f45698d70ed7dc8e7702723f8cc950c0
David de Lusenet <david@lusenet.nl>
2019-10-22 16:22:45 +0200

    Implement new theming setup in Chip molecule

--- src/app/components/molecules/Chip/Chip.sc.js
+++ src/app/components/molecules/Chip/Chip.sc.js
@@ -1,44 +1,8 @@
 import { availableTextStyles, textStyling } from '../../../styles/theme/textStyles';
-import {
-    black,
-    grey10,
-    grey100,
-    grey2,
-    grey5,
-    grey75,
-    white,
-} from '../../../styles/colors/colors';
-import { borderRadius, spacingUnit } from '../../../styles/theme/layout';
-import { colorPrimaryHover, getThemeValue, themeModes } from '../../../styles/theme/theme';
 import styled, { css } from 'styled-components';
 import rippleEffect from '../../../styles/mixins/rippleEffect';
 import setBoxSizing from '../../../styles/mixins/setBoxSizing';
-import styledTheming from 'styled-theming';
 import transitionEffect from '../../../styles/mixins/transitionEffect';
-
-const chipBackgroundColor = styledTheming('mode', {
-    [themeModes.basic]: ({ theme }) => getThemeValue(theme, 'chipBackgroundColor', grey10),
-    [themeModes.dark]: black,
-    [themeModes.light]: grey2,
-});
-
-const chipBackgroundColorDeselected = styledTheming('mode', {
-    [themeModes.basic]: ({ theme }) => getThemeValue(theme, 'chipBackgroundColorDeselected', 'transparent'),
-    [themeModes.dark]: grey75,
-    [themeModes.light]: 'transparent',
-});
-
-const chipBackgroundColorHover = styledTheming('mode', {
-    [themeModes.basic]: ({ theme }) => getThemeValue(theme, 'chipBackgroundColorHover', grey5),
-    [themeModes.dark]: grey75,
-    [themeModes.light]: grey10,
-});
-
-const chipColor = styledTheming('mode', {
-    [themeModes.basic]: ({ theme }) => getThemeValue(theme, 'chipColor', grey100),
-    [themeModes.dark]: white,
-    [themeModes.light]: black,
-});
 
 export const StyledChip = styled.button`
     ${setBoxSizing()};
@@ -50,35 +14,33 @@ export const StyledChip = styled.button`
     appearance: none;
     position: relative;
     outline: none;
-    border: 1px solid ${chipColor};
-    border-radius: ${borderRadius};
-    background-color: ${chipBackgroundColorDeselected};
+    border: 1px solid ${({ theme }) => theme.colorDark.light};
+    border-radius: 8px;
+    background-color: transparent;
     cursor: pointer;
-    padding: calc(${spacingUnit} / 2) ${spacingUnit};
-    min-height: calc(${spacingUnit} * 4);
+    padding: ${({ theme }) => `calc(${theme.spacingUnit} / 2) ${theme.spacingUnit}`};
+    min-height: ${({ theme }) => `calc(${(theme.spacingUnit)} * 4)`};
     overflow: hidden;
-    color: ${chipColor};
+    color: ${({ theme }) => theme.colorDark.light};
 
     ${({ isSelected }) => isSelected && css`
-        background-color: ${chipBackgroundColor};
+        background-color: ${({ theme }) => theme.colorMedium.main};
     `};
 
     ${({ isDisabled }) => isDisabled && css`
         pointer-events: none;
-        color: ${chipColor};
     `};
 
     &:after {
-        ${rippleEffect(colorPrimaryHover)}
+        ${({ theme }) => rippleEffect(theme.colorSecondary.main)};
     }
 
     &:active,
     &:hover {
-        background-color: ${chipBackgroundColor};
-        color: ${chipColor};
+        background-color: ${({ theme }) => theme.colorMedium.main};
 
         ${({ isSelected }) => isSelected && css`
-            background-color: ${chipBackgroundColorHover};
+            background-color: ${({ theme }) => theme.colorLight.dark};
         `};
     }
 
