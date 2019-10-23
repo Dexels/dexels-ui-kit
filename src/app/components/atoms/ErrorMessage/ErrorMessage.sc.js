62dc9139dcf4915ed526f992a5cd302079f82b32
David de Lusenet <david@lusenet.nl>
2019-10-23 13:49:36 +0200

    Remove styled-theming from ErrorMessage atom

--- src/app/components/atoms/ErrorMessage/ErrorMessage.sc.js
+++ src/app/components/atoms/ErrorMessage/ErrorMessage.sc.js
@@ -1,10 +1,9 @@
 import { availableTextStyles, textStyling } from '../../../styles/theme/textStyles';
-import { colorError } from '../../../styles/theme/theme';
 import styled from 'styled-components';
 
 export const StyledErrorMessage = styled.div`
     ${textStyling(availableTextStyles().caption)};
-    color: ${colorError};
+    color: ${({ theme }) => theme.colorError.main};
 `;
 
 export default StyledErrorMessage;
