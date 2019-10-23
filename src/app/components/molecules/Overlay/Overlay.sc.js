b4e8db66a3545bed2e55890c3645fd2ba5cd981e
David de Lusenet <david@lusenet.nl>
2019-10-23 14:48:57 +0200

    Remove styled-theming from Overlay

--- src/app/components/molecules/Overlay/Overlay.sc.js
+++ src/app/components/molecules/Overlay/Overlay.sc.js
@@ -1,13 +1,5 @@
-import { getThemeValue, themeModes } from '../../../styles/theme/theme';
 import styled, { css } from 'styled-components';
 import setBoxSizing from '../../../styles/mixins/setBoxSizing';
-import styledTheming from 'styled-theming';
-
-const overlayOpacity = styledTheming('mode', {
-    [themeModes.basic]: ({ theme }) => getThemeValue(theme, 'overlayOpacity', 0.4),
-    [themeModes.dark]: 0.6,
-    [themeModes.light]: 0.3,
-});
 
 export const StyledOverlay = styled.div`
     ${setBoxSizing()};
@@ -16,21 +8,20 @@ export const StyledOverlay = styled.div`
     z-index: 1;
     overflow: auto;
 
-    ${({ isFullscreen }) => isFullscreen && css`
+    ${({ height, isFullscreen, width }) => (isFullscreen ? css`
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-    `};
-
-    ${({ height, isFullscreen, width }) => !isFullscreen && css`
+    ` : css`
         margin: auto;
         width: ${width};
         height: ${height};
-    `};
+    `)};
 
-    ${({ isVisible }) => isVisible && css`
-        background-color: rgba(0, 0, 0, ${overlayOpacity});
+    ${({ isVisible, theme }) => isVisible && css`
+        background-color: ${theme.colorDark.dark};
+        opacity: 0.4;
     `};
 `;
 
