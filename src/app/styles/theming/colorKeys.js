e4ccce5bc4ace0815082c6ffe913b891a78823aa
David de Lusenet <david@lusenet.nl>
2019-10-28 11:34:45 +0100

    Add colorKeys

---
+++ src/app/styles/theming/colorKeys.js
@@ -0,0 +1,22 @@
+export const colorKeys = [
+    'colorBodyText',
+    'colorDisabled',
+    'colorError',
+    'colorHeaderText',
+    'colorPrimary',
+    'colorSecondary',
+    'colorTertiary',
+    'colorValid',
+    'colorWarning',
+    'shade1',
+    'shade2',
+    'shade3',
+    'shade4',
+    'shade5',
+    'shade6',
+    'shade7',
+    'shade8',
+    'shade9',
+];
+
+export default colorKeys;
