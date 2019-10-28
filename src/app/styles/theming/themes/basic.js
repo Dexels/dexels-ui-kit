b6af2080c2124a60821557ec2ce764581f3e83d5
David de Lusenet <david@lusenet.nl>
2019-10-28 12:10:37 +0100

    Remove unnecessary comment from basic.js

--- src/app/styles/theming/themes/basic.js
+++ src/app/styles/theming/themes/basic.js
@@ -1,23 +1,4 @@
 import mapArrayToObject from '../../../utils/mapArrayToObject';
-// This is how the colors are being called in the design:
-// black = #000000
-// blue10 = #DFF3FE
-// blue25 = #BFE7FE
-// blue50 = #80CFFE
-// blue100 = #009FFD
-// purple50 = #9BA1C9
-// purple100 = #3D4A9A
-// green = #2DD67B
-// orange = #EB6500
-// red = #F94E4E
-// sl2 = #FAFBFC
-// sl5 = #F2F4F6
-// sl10 = #E5E9ED
-// sl25 = #BFC8D3
-// sl50 = #7F91A8
-// sl75 = #3F5A7C
-// sl100 = #002451
-// white = #FFFFFF
 
 /* eslint-disable sort-keys */
 const themeBasic = {
