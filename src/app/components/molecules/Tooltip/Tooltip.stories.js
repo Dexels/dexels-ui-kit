cb07270c916353c45a6c14d18a1b3347fa8d8815
Maria Papadaki <mpapadaki@dexels.com>
2019-11-01 10:32:58 +0100

    Moved css for story buttons inside story file

--- src/app/components/molecules/Tooltip/Tooltip.stories.js
+++ src/app/components/molecules/Tooltip/Tooltip.stories.js
@@ -1,34 +1,91 @@
-import './Tooltip.css';
+import Button from '../Button/Button';
 import React from 'react';
 import Tooltip from './Tooltip';
 
 export default { title: 'molecules/Tooltip' };
 
-export const Configurable = () => {
-    const createBtn = (id, text, delay = false) => (
+export const Configurable = () => (
+    <>
         <button
-            data-tooltip-component={text}
-            data-tooltip-delay={delay}
-            id={id}
+            data-tooltip-component={'click 1'}
+            data-tooltip-delay
+            id={'btnLeft'}
             onBlur={() => {}}
             onFocus={() => {}}
+            style={{
+                left: '-60px',
+                position: 'absolute',
+                top: '0px',
+            }}
             type={'button'}
         >
-            {text}
+            {'click 1'}
         </button>
-    );
-
-    return (
-        <>
-            {createBtn('btnLeft', 'click 1', true)}
-            {createBtn('btnRight', 'click 2')}
-            {createBtn('btnBtmR', 'click 3')}
-            {createBtn('btnCenter', 'click 4')}
-            <Tooltip
-                elevation={Tooltip.defaultProps.elevation}
-                transitionDuration={Tooltip.defaultProps.transitionDuration}
-                transitionEasing={Tooltip.defaultProps.transitionEasing}
-            />
-        </>
-    );
-};
+        <button
+            data-tooltip-component={'click 2'}
+            data-tooltip-delay={false}
+            id={'btnRight'}
+            onBlur={() => {}}
+            onFocus={() => {}}
+            style={{
+                float: 'right',
+                position: 'absolute',
+                right: '-80px',
+                top: '10px',
+            }}
+            type={'button'}
+        >
+            {'click 2'}
+        </button>
+        <div
+            data-tooltip-component={'tooltip'}
+            data-tooltip-delay={false}
+            style={{
+                left: '400px',
+                position: 'absolute',
+                top: '100px',
+            }}
+        >
+            <Button
+                onClick={() => {}}
+                size="SMALL"
+            >
+                {'Click me!'}
+            </Button>
+        </div>
+        <button
+            data-tooltip-component={'click 3'}
+            data-tooltip-delay={false}
+            id={'btnBtmR'}
+            onBlur={() => {}}
+            onFocus={() => {}}
+            style={{
+                position: 'absolute',
+                right: '70px',
+            }}
+            type={'button'}
+        >
+            {'click 3'}
+        </button>
+        <button
+            data-tooltip-component={'click 4'}
+            data-tooltip-delay={false}
+            id={'btnCenter'}
+            onBlur={() => {}}
+            onFocus={() => {}}
+            style={{
+                left: '150px',
+                position: 'absolute',
+                top: '300px',
+            }}
+            type={'button'}
+        >
+            {'click 4'}
+        </button>
+        <Tooltip
+            elevation={Tooltip.defaultProps.elevation}
+            transitionDuration={Tooltip.defaultProps.transitionDuration}
+            transitionEasing={Tooltip.defaultProps.transitionEasing}
+        />
+    </>
+);
