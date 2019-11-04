66562dd56e3548734dd099db04573a4eab843c9c
David de Lusenet <david@lusenet.nl>
2019-11-04 11:25:08 +0100

    Update Dropdown stories

--- src/app/components/molecules/Dropdown/Dropdown.stories.js
+++ src/app/components/molecules/Dropdown/Dropdown.stories.js
@@ -5,7 +5,7 @@ import Dropdown from './Dropdown';
 export default { title: 'molecules/Dropdown' };
 
 export const ConfigurableCompactVariant = () => {
-    const placeholder = 'Select your fruit';
+    const placeholder = 'Select the best fruit';
     const fruits = ['Banana', 'Apple', 'Orange', 'Pear', 'Strawberry'];
     const [value, setValue] = useState(placeholder);
 
@@ -42,9 +42,9 @@ export const ConfigurableCompactVariant = () => {
 };
 
 export const ConfigurableOutlineVariant = () => {
-    const placeholder = 'Select your fruit';
+    const placeholder = 'Select a fruit';
     const fruits = ['Banana', 'Apple', 'Orange', 'Pear', 'Strawberry'];
-    const [value, setValue] = useState(fruits[0]);
+    const [value, setValue] = useState(placeholder);
 
     const optionArray = fruits.map((fruit, index) => (
         <option disabled={index === 2} key={fruit} value={fruit}>
@@ -54,19 +54,17 @@ export const ConfigurableOutlineVariant = () => {
 
     return (
         <>
-            <p>
-                {'What is the best fruit?'}
-            </p>
             <Dropdown
                 errorMessage={text('Error message', 'Everything is broken, oops')}
                 hasError={boolean('Has error', Dropdown.defaultProps.hasError)}
                 isDisabled={boolean('Is disabled', Dropdown.defaultProps.isDisabled)}
                 isValid={boolean('Is valid', Dropdown.defaultProps.isValid)}
-                label="Select your fruit"
+                label="Your favorite fruit"
                 name="the-best-fruit"
                 onChange={(event) => {
                     setValue(event.currentTarget.value);
                 }}
+                placeholder={placeholder}
                 value={value}
                 variant={Dropdown.variants.OUTLINE}
             >
