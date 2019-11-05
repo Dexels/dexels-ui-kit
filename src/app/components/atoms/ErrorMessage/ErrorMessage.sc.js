d89ec53db00e48a770ef46bd821ef3d8b7277526
David de Lusenet <david@lusenet.nl>
2019-11-05 11:31:07 +0100

    Add default theme to ErrorMessage

--- src/app/components/atoms/ErrorMessage/ErrorMessage.sc.js
+++ src/app/components/atoms/ErrorMessage/ErrorMessage.sc.js
@@ -1,8 +1,22 @@
+import basicTheme from '../../../styles/theming/themes/basic';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 export const StyledErrorMessage = styled.div`
     ${({ theme }) => theme.textStyling(theme.availableTextStyles().caption)};
     color: ${({ theme }) => theme.colorInvalid};
 `;
+
+StyledErrorMessage.propTypes = {
+    theme: PropTypes.shape({
+        availableTextStyles: PropTypes.func.isRequired,
+        colorInvalid: PropTypes.string.isRequired,
+        textStyling: PropTypes.func.isRequired,
+    }),
+};
+
+StyledErrorMessage.defaultProps = {
+    theme: basicTheme,
+};
 
 export default StyledErrorMessage;
