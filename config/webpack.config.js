e3d26daba7b325785736fdb39b109aa52d8f6085
David de Lusenet <david@lusenet.nl>
2019-11-06 11:12:20 +0100

    Remove externals + bump version

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,13 +10,11 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 module.exports = (env = {}) => ({
     entry: resolve(__dirname, `${libPath}/index.js`),
     externals: {
-        // '@babel/runtime': '@babel/runtime',
         moment: 'moment',
         'prop-types': 'prop-types',
         react: 'react',
         'react-dates': 'react-dates',
         'react-dom': 'react-dom',
-        // 'react-with-direction': 'react-with-direction',
         'styled-components': 'styled-components',
     },
     mode: 'production',
