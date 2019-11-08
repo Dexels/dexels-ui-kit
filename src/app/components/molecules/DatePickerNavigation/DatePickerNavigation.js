3cdb5db15a7582d98b5c2c9ce37c4f0e52e06ff1
David de Lusenet <david@lusenet.nl>
2019-11-08 07:53:11 +0100

    Add DatePickerNavigation molecule

---
+++ src/app/components/molecules/DatePickerNavigation/DatePickerNavigation.js
@@ -0,0 +1,89 @@
+import { Date, DropdownWrapper, StyledDatePickerNavigation } from './DatePickerNavigation.sc';
+import Dropdown from '../Dropdown/Dropdown';
+import moment from 'moment';
+import momentPropTypes from 'react-moment-proptypes';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const DatePickerNavigation = ({
+    hasYearSelector,
+    labelMonth,
+    labelYear,
+    month,
+    onMonthSelect,
+    onYearSelect,
+    yearCount,
+}) => {
+    if (!hasYearSelector) {
+        return (
+            <Date>
+                {`${month.format('MMMM')} ${month.year()}`}
+            </Date>
+        );
+    }
+
+    const monthArray = moment.months().map((label, value) => (
+        <option key={label} value={value}>
+            {label}
+        </option>
+    ));
+
+    const currentYear = moment().year();
+    const yearArray = [];
+
+    for (let i = currentYear - yearCount; i <= currentYear; i += 1) {
+        yearArray.push(
+            <option key={i} value={i}>
+                {i}
+            </option>,
+        );
+    }
+
+    return (
+        <StyledDatePickerNavigation>
+            <DropdownWrapper>
+                <Dropdown
+                    label={labelMonth}
+                    name="month-dropdown"
+                    onChange={(e) => {
+                        onMonthSelect(month, e.target.value);
+                    }}
+                    value={month.month()}
+                    variant={Dropdown.variants.OUTLINE}
+                >
+                    {monthArray}
+                </Dropdown>
+            </DropdownWrapper>
+            <DropdownWrapper>
+                <Dropdown
+                    label={labelYear}
+                    name="year-dropdown"
+                    onChange={(e) => {
+                        onYearSelect(month, e.target.value);
+                    }}
+                    value={month.year()}
+                    variant={Dropdown.variants.OUTLINE}
+                >
+                    {yearArray}
+                </Dropdown>
+            </DropdownWrapper>
+        </StyledDatePickerNavigation>
+    );
+};
+
+DatePickerNavigation.propTypes = {
+    hasYearSelector: PropTypes.bool.isRequired,
+    labelMonth: PropTypes.string,
+    labelYear: PropTypes.string,
+    month: momentPropTypes.momentObj.isRequired,
+    onMonthSelect: PropTypes.func.isRequired,
+    onYearSelect: PropTypes.func.isRequired,
+    yearCount: PropTypes.number.isRequired,
+};
+
+DatePickerNavigation.defaultProps = {
+    labelMonth: '',
+    labelYear: '',
+};
+
+export default DatePickerNavigation;
---
+++ src/app/components/molecules/DatePickerNavigation/DatePickerNavigation.sc.js
@@ -0,0 +1,43 @@
+import { themeBasic, themePropTypes } from '../../../styles/theming/themes/basic';
+import styled from 'styled-components';
+
+export const StyledDatePickerNavigation = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+`;
+
+export const DropdownWrapper = styled.div`
+    flex: 0 1 auto;
+    width: 50%;
+
+    &:first-of-type {
+        margin: ${({ theme }) => theme.spacing(0, 0.75, 0, 0)};
+    }
+
+    &:last-of-type {
+        margin: ${({ theme }) => theme.spacing(0, 0, 0, 0.75)};
+    }
+`;
+
+DropdownWrapper.propTypes = {
+    theme: themePropTypes,
+};
+
+DropdownWrapper.defaultProps = {
+    theme: themeBasic,
+};
+
+export const Date = styled.p`
+    ${({ theme }) => theme.textStyling(theme.availableTextStyles().h2)}
+    margin: 0;
+    text-transform: capitalize;
+    color: ${({ theme }) => theme.colorHeaderText.primary};
+`;
+
+Date.propTypes = {
+    theme: themePropTypes,
+};
+
+Date.defaultProps = {
+    theme: themeBasic,
+};
