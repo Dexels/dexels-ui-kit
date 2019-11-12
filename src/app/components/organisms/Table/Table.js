d2b06c9895cc1a378133b38afaca3fb49dabb459
David de Lusenet <david@lusenet.nl>
2019-11-12 09:28:31 +0100

    Destructure instance

--- src/app/components/organisms/Table/Table.js
+++ src/app/components/organisms/Table/Table.js
@@ -31,82 +31,91 @@ const Table = ({
     onRowClick,
     pagingComponent,
     texts,
-}) => (
-    <>
-        {caption && (
-            <TableCaption>
-                {caption}
-            </TableCaption>
-        )}
-        <StyledTable
-            debug={debug}
-            elevation={elevation}
-            isFullWidth={isFullWidth}
-            {...instance.getTableProps()}
-        >
-            <TableHead>
-                {instance.headerGroups.map((headerGroup) => (
-                    <TableHeaderRow key={headerGroup} {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                            <TableHeaderCell
-                                key={column}
-                                {...column.getHeaderProps(column.getSortByToggleProps({
-                                    title: column.canSort
-                                        ? `${texts.toggleSortTooltip} ${column.render('Header')}`
-                                        : '',
-                                }))}
-                            >
-                                {column.render('Header')}
-                                <IconWrapper isSorted={column.isSorted}>
-                                    {renderSortIcon(column, hasUnsortedStateIcon)}
-                                </IconWrapper>
-                            </TableHeaderCell>
-                        ))}
-                    </TableHeaderRow>
-                ))}
-            </TableHead>
-            <TableBody {...instance.getTableBodyProps()}>
-                {/* USE A CONST (SEE TOP OF FILE) TO DETERMINE CORRECT DATA SOURCE FOR READING (PAGE OR ROWS) */}
-                {dataSource(instance, pagingComponent !== undefined).map((row) => instance.prepareRow(row) || (
-                    <TableRow
-                        isClickable={onRowClick !== undefined}
-                        onClick={onRowClick
-                            ? (event) => {
-                                onRowClick(event, row);
-                            }
-                            : undefined}
-                        {...row.getRowProps()}
-                    >
-                        {row.cells.map((cell) => (
-                            <TableCell
-                                isClickable={cell.column.onClick !== undefined}
-                                key={cell}
-                                onClick={cell.column.onClick
-                                    ? (event) => {
-                                        cell.column.onClick(cell, row, event);
-                                    }
-                                    : undefined}
-                                {...cell.getCellProps()}
-                            >
-                                {cell.render('Cell')}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                ))}
-            </TableBody>
-            {footerComponent && (
-                <TableFooter>
-                    {footerComponent}
-                </TableFooter>
+}) => {
+    const {
+        getTableBodyProps,
+        getTableProps,
+        headerGroups,
+        prepareRow,
+    } = instance;
+
+    return (
+        <>
+            {caption && (
+                <TableCaption>
+                    {caption}
+                </TableCaption>
             )}
-        </StyledTable>
-        {pagingComponent && (
-            <Paging>
-                {pagingComponent}
-            </Paging>
-        )}
-    </>
-);
+            <StyledTable
+                debug={debug}
+                elevation={elevation}
+                isFullWidth={isFullWidth}
+                {...getTableProps()}
+            >
+                <TableHead>
+                    {headerGroups.map((headerGroup) => (
+                        <TableHeaderRow key={headerGroup} {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <TableHeaderCell
+                                    key={column}
+                                    {...column.getHeaderProps(column.getSortByToggleProps({
+                                        title: column.canSort
+                                            ? `${texts.toggleSortTooltip} ${column.render('Header')}`
+                                            : '',
+                                    }))}
+                                >
+                                    {column.render('Header')}
+                                    <IconWrapper isSorted={column.isSorted}>
+                                        {renderSortIcon(column, hasUnsortedStateIcon)}
+                                    </IconWrapper>
+                                </TableHeaderCell>
+                            ))}
+                        </TableHeaderRow>
+                    ))}
+                </TableHead>
+                <TableBody {...getTableBodyProps()}>
+                    {/* USE A CONST (SEE TOP OF FILE) TO DETERMINE CORRECT DATA SOURCE FOR READING (PAGE OR ROWS) */}
+                    {dataSource(instance, Boolean(pagingComponent)).map((row) => prepareRow(row) || (
+                        <TableRow
+                            isClickable={Boolean(onRowClick)}
+                            onClick={onRowClick
+                                ? (event) => {
+                                    onRowClick(event, row);
+                                }
+                                : undefined}
+                            {...row.getRowProps()}
+                        >
+                            {row.cells.map((cell) => (
+                                <TableCell
+                                    isClickable={Boolean(cell.column.onClick)}
+                                    key={cell}
+                                    onClick={cell.column.onClick
+                                        ? (event) => {
+                                            cell.column.onClick(cell, row, event);
+                                        }
+                                        : undefined}
+                                    {...cell.getCellProps()}
+                                >
+                                    {cell.render('Cell')}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+                {footerComponent && (
+                    <TableFooter>
+                        {footerComponent}
+                    </TableFooter>
+                )}
+            </StyledTable>
+            {pagingComponent && (
+                <Paging>
+                    {pagingComponent}
+                </Paging>
+            )}
+        </>
+    );
+};
 
 Table.elevations = TABLE_ELEVATIONS;
 Table.pageSizes = TABLE_PAGE_SIZES;
