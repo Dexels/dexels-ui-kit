df52ac169443a8c1fc13001a5cccae8568819713
David de Lusenet <david@lusenet.nl>
2019-11-18 12:30:09 +0100

    Add invertcolor to function-name-case rule expections

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -23,7 +23,7 @@ module.exports = {
         'function-comma-space-after': 'always-single-line',
         'function-comma-space-before': 'never',
         'function-name-case': ['lower', {
-            ignoreFunctions: ['/^get.*$/'],
+            ignoreFunctions: ['/^get.*$/', 'invertcolor'],
         }],
         'function-parentheses-space-inside': 'never',
         indentation: 4,
