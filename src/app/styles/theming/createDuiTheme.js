33b1570953708b9364c02e411c0fccc1a72627f0
Erik Versteeg <erik@dexels.com>
2019-11-18 09:02:54 +0100

    Merged master

--- src/app/styles/theming/createDuiTheme.js
+++ src/app/styles/theming/createDuiTheme.js
@@ -1,5 +1,3 @@
-import { cloneObject } from '../../utils/functions/objectFunctions';
-
 const checkIfPropertyExists = (object, property, completePropertyName) => {
     if (!Object.prototype.hasOwnProperty.call(object, property)) {
         throw Error(`The theme you provided doesn't have a '${completePropertyName || property}' property on it`);
@@ -7,10 +5,9 @@ const checkIfPropertyExists = (object, property, completePropertyName) => {
 };
 
 const createDuiTheme = (baseTheme, overrides) => {
-    const theme = cloneObject(baseTheme);
-    // const theme = {
-    //     ...baseTheme,
-    // };
+    const theme = {
+        ...baseTheme,
+    };
 
     Object.keys(overrides).forEach((property1) => {
         checkIfPropertyExists(baseTheme, property1);
