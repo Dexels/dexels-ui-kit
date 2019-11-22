a13bca18f0b474036ef9c334965a9330164da8bf
David de Lusenet <david@lusenet.nl>
2019-11-22 14:44:25 +0100

    Add types + add @types/react peer dependency

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,33 +1,54 @@
-// Type definitions for dexels-ui-kit 1.0.29
+// Type definitions for dexels-ui-kit 1.0.42
 // Project: dexels-ui-kit
 // Definitions by: David de Lusenet <https://github.com/daviddelusenet>
+// TypeScript Version: 3.7.2
 
+// References to types which our library depends on
+/// <reference types="react" />
+
+// Set up the project namespace, should be the same as the output.library field in the webpack config
 export as namespace DexelsUIKit;
 
-/*~ If this module has methods, declare them as functions like so.
- */
+// Typings for functions/variables exported by the DexelsUIKit package
 export function createDuiTheme(baseTheme: object, overrides: object): object;
 
-/*~ You can declare types that are available via importing the module */
-// export interface someType {
-//     name: string;
-//     length: number;
-//     extras?: string[];
-// }
+export const themeBasic: {
+    [key: string]: any;
+    shades: {
+        one: string;
+        two: string;
+        three: string;
+        four: string;
+        five: string;
+        six: string;
+        seven: string;
+        eight: string;
+        nine: string;
+    }
+    colorPrimary: string;
+    colorSecondary: string;
+    colorTertiary: string;
+    colorAlert: string;
+    colorInvalid: string;
+    colorValid: string;
+    spacing: (factor1: number, factor2?: number, factor3?: number, factor4?: number) => string;
+}
 
-/*~ You can declare properties of the module using const, let, or var */
-// export const myField: number;
+// Typings for components exported by the DexelsUIKit package
+export type elevations = 'LEVEL_0' | 'LEVEL_1' | 'LEVEL_2' | 'LEVEL_3' | 'LEVEL_4' | 'LEVEL_6' | 'LEVEL_8' | 'LEVEL_12' | 'LEVEL_16' | 'LEVEL_24';
+export type positions = 'BOTTOM_CENTER' | 'BOTTOM_LEFT' | 'BOTTOM_RIGHT' | 'MIDDLE_CENTER' | 'MIDDLE_LEFT' | 'MIDDLE_RIGHT' | 'TOP_CENTER' | 'TOP_LEFT' | 'TOP_RIGHT';
 
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-// export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-//     export function foo(): void;
-// }
+export interface ErrorMessageProps {
+    children: React.ReactNode;
+}
+
+export const ErrorMessage: React.FunctionComponent<ErrorMessageProps>;
+
+export interface CardProps {
+    children: React.ReactNode,
+    elevation?: elevations,
+    hasBorderRadius?: boolean,
+    position?: positions,
+}
+
+export const Card: React.FunctionComponent<CardProps>;
