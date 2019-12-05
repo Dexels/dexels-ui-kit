afea333ebbba488e2dde81c43db8c4274049f74a
David de Lusenet <david@lusenet.nl>
2019-12-05 12:05:18 +0100

    Add first setup of Loader component

---
+++ src/app/components/molecules/Loader/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledLoader } from './Loader.sc';
+
+const Loader = () => (
+    <StyledLoader>
+        <div />
+    </StyledLoader>
+);
+
+export default Loader;
---
+++ src/app/components/molecules/Loader/Loader.sc.js
@@ -0,0 +1,68 @@
+import styled, { keyframes } from 'styled-components';
+
+const loaderAnimation = keyframes`
+    0% {
+        transform: scale(0.90);
+    }
+
+    5% {
+        transform: scale(1.15);
+    }
+
+    39% {
+        transform: scale(0.80);
+    }
+
+    45% {
+        transform: scale(1.05);
+    }
+
+    60% {
+        transform: scale(0.90);
+    }
+
+    100% {
+        transform: scale(0.85);
+    }
+`;
+
+export const StyledLoader = styled.div`
+    position: relative;
+    transform: rotate(45deg);
+    transform-origin: ${({ theme }) => theme.spacing(2.5, 2.5)};
+    width: ${({ theme }) => theme.spacing(5)};
+    height: ${({ theme }) => theme.spacing(5)};
+
+    div {
+        position: absolute;
+        top: ${({ theme }) => theme.spacing(2)};
+        left: ${({ theme }) => theme.spacing(2)};
+        border-radius: ${({ theme }) => theme.spacing(0, 0, 0.375)};
+        background: ${({ theme }) => theme.background.primary};
+        width: ${({ theme }) => theme.spacing(2)};
+        height: ${({ theme }) => theme.spacing(2)};
+        animation: ${loaderAnimation} 2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
+
+        &::after,
+        &::before {
+            display: block;
+            position: absolute;
+            background: ${({ theme }) => theme.background.primary};
+            width: ${({ theme }) => theme.spacing(2)};
+            height: ${({ theme }) => theme.spacing(2)};
+            content: '';
+        }
+
+        &::before {
+            left: -${({ theme }) => theme.spacing(1.5)};
+            border-radius: 50% 0 0 50%;
+        }
+
+        &::after {
+            top: -${({ theme }) => theme.spacing(1.5)};
+            border-radius: 50% 50% 0 0;
+        }
+    }
+`;
+
+export default StyledLoader;
