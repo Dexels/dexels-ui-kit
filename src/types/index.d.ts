5d0863ddcfac400b8b462ae419db618b0c2df209
David de Lusenet <david@lusenet.nl>
2019-12-10 14:13:55 +0100

    Bump version

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dexels-ui-kit 2.0.35
+// Type definitions for dexels-ui-kit 2.0.36
 // Project: dexels-ui-kit
 // Definitions by: David de Lusenet <https://github.com/daviddelusenet>
 // TypeScript Version: 3.7.3
