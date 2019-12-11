4f35d8bf2539f84bb753f028e72c4d5a52b43b7f
David de Lusenet <david@lusenet.nl>
2019-12-11 16:11:49 +0100

    Fix bug in getStatusIndicator + bump version

--- src/app/styles/mixins/getStatusIndicator.js
+++ src/app/styles/mixins/getStatusIndicator.js
@@ -3,6 +3,5 @@ import getStatusColor from './getStatusColor';
 import { STATUS_INDICATOR_SIZES } from '../../components/atoms/StatusIndicator/StatusIndicator.consts';
 
 export default (status, theme, placement, size) => (css`
-    border-color: ${getStatusColor(status, theme)};
-    border-${placement}: ${theme.spacing(size === STATUS_INDICATOR_SIZES.LARGE ? 1 : 0.5)} solid;
+    border-${placement}: ${theme.spacing(size === STATUS_INDICATOR_SIZES.LARGE ? 1 : 0.5)} solid ${getStatusColor(status, theme)};
 `);
