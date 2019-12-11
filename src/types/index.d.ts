6a13dc13324ec239f1e724f0519e9363906e5159
David de Lusenet <david@lusenet.nl>
2019-12-11 09:31:26 +0100

    Update peer dependencies and bump version

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dexels-ui-kit 2.0.40
+// Type definitions for dexels-ui-kit 2.0.41
 // Project: dexels-ui-kit
 // Definitions by: David de Lusenet <https://github.com/daviddelusenet>
 // TypeScript Version: 3.7.3
