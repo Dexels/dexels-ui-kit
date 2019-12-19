559aa5898db82710f17691fe46ab0f28991841d3
David de Lusenet <david@lusenet.nl>
2019-12-19 09:58:23 +0100

    Fix broken import

--- src/.storybook/addons.js
+++ src/.storybook/addons.js
@@ -4,4 +4,4 @@ import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-notes/register-panel';
-import 'storybook-addon-styled-component-theme/dist/src/register';
+import 'storybook-addon-styled-component-theme/dist/register';
