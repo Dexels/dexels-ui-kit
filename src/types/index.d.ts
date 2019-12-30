02c4d4d243aa189c07b27abf44144c2c1fa4b198
Erik Versteeg <erik@dexels.com>
2019-12-30 11:34:01 +0100

    Bumped packages

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for dexels-ui-kit 2.0.57
+// Type definitions for dexels-ui-kit 2.0.58
 // Project: dexels-ui-kit
 // Definitions by: David de Lusenet <https://github.com/daviddelusenet>
 // TypeScript Version: 3.7.3
