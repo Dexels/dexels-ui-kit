1494b6d0a21c2d97972807bedf5265dbbd597fd7
Erik Versteeg <erik@dexels.com>
2020-02-10 11:32:39 +0100

    Added some export to react-table

--- src/typings/react-table.d.ts
+++ src/typings/react-table.d.ts
@@ -1,12 +1,18 @@
 import {
+    ColumnInstance,
     IdType,
     SortingRule,
+    UseFiltersColumnProps,
+    UseGroupByColumnProps,
     UsePaginationInstanceProps,
+    UseResizeColumnsColumnProps,
+    UseSortByColumnProps,
     UseTableInstanceProps,
     UseTableOptions,
 } from 'react-table';
 
 declare module 'react-table' {
+    /* eslint-disable @typescript-eslint/no-explicit-any */
     export interface TableInstance<D extends object = {}>
         extends Omit<TableOptions<D>, 'columns' | 'pageCount'>,
         UsePaginationInstanceProps<D>,
@@ -20,4 +26,10 @@ declare module 'react-table' {
         pageIndex: number;
         sortBy: SortingRule<D>[];
     }
+
+    export interface ColumnInstance<D extends object = {}>
+        extends UseFiltersColumnProps<D>,
+        UseGroupByColumnProps<D>,
+        UseResizeColumnsColumnProps<D>,
+        UseSortByColumnProps<D> {}
 }
