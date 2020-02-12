0a70b6e1089117a16d24d2201bc8e8905ecb6a43
David de Lusenet <david.delusenet@cygni.se>
2020-02-12 12:17:30 +0100

    FB: new icon font 12-02-2020 (#114)
    * Update generateIconTypes so that the output is perfect
    
    * Add icon font
    
    * Update dependencies
    
    * Downgrade versions
    
    * Bump version
    
    * Update dependencies again since this had nothing to do with not being able to publish
    
    * Converted EOL
    
    Co-authored-by: Erik Versteeg <1769015+eversteeg@users.noreply.github.com>

--- scripts/generateIconTypes.js
+++ scripts/generateIconTypes.js
@@ -3,11 +3,10 @@ const selection = require('../src/app/styles/fonts/iconfont/selection.json');
 const icons = selection.icons.map(({ properties: { name } }) => name.toUpperCase()).sort();
 
 const iconTypes = `
-    export enum IconType {
-    ${icons.map(name => `
-        ${name} = '${name}',`).join('\n')}
-    }
-`;
+export enum IconType {
+${icons.map(name => `
+    ${name} = '${name}',`).join('\n')}
+}`;
 
 // Out
 console.log(iconTypes);
