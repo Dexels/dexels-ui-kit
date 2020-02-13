0a173eac3163c7afaef979e7721d65b56aa6206c
Erik Versteeg <erik@dexels.com>
2020-02-13 14:14:20 +0100

    WIP

--- src/app/components/organisms/Table/mockup/createTable.ts
+++ src/app/components/organisms/Table/mockup/createTable.ts
@@ -5,24 +5,17 @@ import {
     useSortBy,
     useTable,
 } from 'react-table';
-// import { customSortByCaseInsensitive } from '../utils/tableFunctions';
 
 export const createTable = <T extends object>(
     columns: Column<T>[],
     data: T[],
     initialState: object,
-    disableMultiSort = false,
-    disableSorting = false,
-    // orderByFn = customSortByCaseInsensitive,
 ): TableInstance => (
     useTable(
         {
             columns,
             data,
-            disableMultiSort,
-            disableSorting,
             initialState,
-            // orderByFn,
         },
         useSortBy,
         usePagination,
