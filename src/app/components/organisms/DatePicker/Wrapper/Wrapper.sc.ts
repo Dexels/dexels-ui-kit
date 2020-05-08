987282585849fa4add84fcc420c46ce0bd5816e8
ayalalevinger <alevinger@dexels.com>
2020-05-08 10:57:39 +0200

    workaround brouwsers lightning placeholder color

--- src/app/components/organisms/DatePicker/Wrapper/Wrapper.sc.ts
+++ src/app/components/organisms/DatePicker/Wrapper/Wrapper.sc.ts
@@ -75,7 +75,8 @@ export const StyledWrapper = styled.div<StyledWrapperProps>`
         color: ${({ theme }): string => theme.colorText.primary};
 
         &::placeholder {
-            color: ${({ theme }): string => theme.shades.four};
+            color: ${({ theme }): string => theme.shades.five};
+            opacity: 1;
         }
     }
 
