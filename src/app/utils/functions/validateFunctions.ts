c8ac6bc7952e47376cd58e7fcd7893dd4e39cf99
Erik Versteeg <erik@dexels.com>
2020-06-15 10:53:25 +0200

    Added validate functions

---
+++ src/app/utils/functions/validateFunctions.ts
@@ -0,0 +1,12 @@
+export const isValidEmail = (value: string): boolean => {
+    // eslint-disable-next-line max-len
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return emailRegex.test(value);
+};
+
+export const isValidPhoneNumber = (value: string): boolean => {
+    const phoneRegExp = /^(\+(([0-9]){1,2})[-.])?((((([0-9]){2,3})[-.]){1,2}([0-9]{4,10}))|([0-9]{10}))$/;
+
+    return phoneRegExp.test(value);
+};
