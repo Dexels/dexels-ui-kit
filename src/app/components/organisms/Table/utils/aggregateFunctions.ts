d5570bb616d502559ae1e122be756308d6824049
ayalalevinger <alevinger@dexels.com>
2020-06-16 13:33:49 +0200

    remove rule no-unsafe-return

--- src/app/components/organisms/Table/utils/aggregateFunctions.ts
+++ src/app/components/organisms/Table/utils/aggregateFunctions.ts
@@ -3,9 +3,11 @@ import { TableData } from '../mockup/tableData';
 import { toMoney } from '../../../../utils/functions/financialFunctions';
 
 export const sum = (data: Row<TableData>[], accessor: string): number => {
-    const values = data.map((row: Row<TableData>) => (row.values[accessor] !== undefined ? row.values[accessor] : 0));
+    const values = data.map((row: Row<TableData>) =>
+        row.values[accessor] !== undefined ? (row.values[accessor] as number | string) : 0
+    );
 
-    const total = values.reduce((accumulator: number, currentValue: number) => {
+    const total = values.reduce((accumulator: number, currentValue: number | string) => {
         return accumulator + toMoney(currentValue).value;
     }, 0);
 
