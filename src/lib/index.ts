0b11bf9494cc911c49ec03c43ccccf5c318b9b72
Erik Versteeg <erik@dexels.com>
2020-07-20 12:54:32 +0200

    Added financial functions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -113,7 +113,14 @@ export { hexToRgb, invertColor } from '../app/utils/functions/colorFunctions';
 export { parseInputValue } from '../app/utils/functions/parseInputValue';
 export { sum } from '../app/components/organisms/Table/utils/aggregateFunctions';
 export { isEmpty, isValidEmail, isValidPhoneNumber } from '../app/utils/functions/validateFunctions';
-export { defaultCurrencySettings, formatMoney, toMoney, toMoneyValue } from '../app/utils/functions/financialFunctions';
+export {
+    defaultCurrencySettings,
+    formatMoney,
+    formatMoneyWithoutSymbol,
+    getCurrencySymbol,
+    toMoney,
+    toMoneyValue,
+} from '../app/utils/functions/financialFunctions';
 export {
     getFileFormats,
     getFileNames,
