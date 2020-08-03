a58f500e136b7146a72c850a00e5dddb7f9f9067
Veronika Demina <Veronika@Dexelss-MacBook-Pro.local>
2020-08-03 13:39:25 +0200

    replace icon in CardNoResults

--- src/app/components/molecules/CardNoResults/CardNoResults.sc.ts
+++ src/app/components/molecules/CardNoResults/CardNoResults.sc.ts
@@ -61,19 +61,6 @@ Left.defaultProps = {
     theme: themeBasic,
 };
 
-export const IconWrapper = styled.div`
-    color: ${({ theme }): string => theme.colorText.primary};
-    font-size: 30px;
-
-    span {
-        display: block;
-    }
-`;
-
-IconWrapper.defaultProps = {
-    theme: themeBasic,
-};
-
 export const Right = styled.div`
     flex: 1 1 auto;
 `;
--- src/app/components/molecules/CardNoResults/CardNoResults.tsx
+++ src/app/components/molecules/CardNoResults/CardNoResults.tsx
@@ -1,7 +1,11 @@
 import { Elevation, IconType } from '../../../types';
-import { Header, IconWrapper, Item, Left, Right, StyledCardNoResults, Title } from './CardNoResults.sc';
+import { Header, Item, Left, Right, StyledCardNoResults, Title } from './CardNoResults.sc';
 import React, { FunctionComponent, ReactNode } from 'react';
-import Icon from '../../atoms/Icon/Icon';
+import { colorKeys } from '../../../styles/theming/colorKeys';
+import { getColorsFromTheme } from '../../../styles/theming/getColorsFromTheme';
+import IconCustomizable from '../IconCustomizable/IconCustomizable';
+import { IconCustomizableSize } from '../IconCustomizable/types';
+import { themeBasic } from '../../../styles/theming/themes/basic';
 
 export interface CardNoResultsProps {
     children?: never;
@@ -22,25 +26,31 @@ export const CardNoResults: FunctionComponent<CardNoResultsProps> = ({
     itemPrefix = '-',
     items = [],
     title,
-}) => (
-    <StyledCardNoResults className={className} elevation={elevation}>
-        <Left>
-            <IconWrapper>
-                <Icon type={iconType} />
-            </IconWrapper>
-        </Left>
-        <Right>
-            <Header>{header}</Header>
-            <Title>{title}</Title>
-            {items.length > 0 &&
-                items.map((item) => (
-                    <Item key={item?.toString()}>
-                        {`${itemPrefix} `}
-                        {item}
-                    </Item>
-                ))}
-        </Right>
-    </StyledCardNoResults>
-);
+}) => {
+    const colors = getColorsFromTheme(themeBasic, colorKeys);
+
+    return (
+        <StyledCardNoResults className={className} elevation={elevation}>
+            <Left>
+                <IconCustomizable
+                    iconColor={colors['colorText-primary']}
+                    iconSize={IconCustomizableSize.SIZE30}
+                    iconType={iconType}
+                />
+            </Left>
+            <Right>
+                <Header>{header}</Header>
+                <Title>{title}</Title>
+                {items.length > 0 &&
+                    items.map((item) => (
+                        <Item key={item?.toString()}>
+                            {`${itemPrefix} `}
+                            {item}
+                        </Item>
+                    ))}
+            </Right>
+        </StyledCardNoResults>
+    );
+};
 
 export default CardNoResults;
