1d5e9768d9ee8f611adbfe17289b2f5345986b89
Veronika Demina <Veronika@Dexelss-MacBook-Pro.local>
2020-08-17 14:24:44 +0200

    add functions to compare arrays

--- src/app/utils/functions/arrayFunctions.ts
+++ src/app/utils/functions/arrayFunctions.ts
@@ -10,4 +10,46 @@ export function cloneArray<T = Record<string, unknown>>(data: T[]): T[] {
     return result;
 }
 
-export default cloneArray;
+export function arraysDifference<T extends Record<string, unknown>>(prevObject: T, nextObject: T): string[] {
+    const prevKeys = Object.keys(prevObject);
+    const keys = Object.keys(nextObject);
+    const differKeys = prevKeys.filter((key) => !keys.includes(key));
+
+    if (differKeys.length > 0) {
+        return differKeys;
+    }
+
+    const differValues = prevKeys.filter((key) => {
+        if (typeof prevObject[key] === 'object') {
+            return JSON.stringify(prevObject[key]) !== JSON.stringify(nextObject[key]) && [];
+        }
+
+        return prevObject[key] !== nextObject[key];
+    });
+
+    return differValues;
+}
+
+export function areArraysEqual<T extends Record<string, unknown>>(prevObject: T, nextObject: T): boolean {
+    const prevKeys = Object.keys(prevObject);
+    const keys = Object.keys(nextObject);
+    const differKeys = prevKeys.filter((key) => !keys.includes(key));
+
+    if (differKeys.length > 0) {
+        return false;
+    }
+
+    const differValues = prevKeys.filter((key) => {
+        if (typeof prevObject[key] === 'object') {
+            return JSON.stringify(prevObject[key]) !== JSON.stringify(nextObject[key]) && [];
+        }
+
+        return prevObject[key] !== nextObject[key];
+    });
+
+    if (differValues.length > 0) {
+        return false;
+    }
+
+    return true;
+}
