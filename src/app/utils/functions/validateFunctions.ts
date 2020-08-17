dbd5abe50ad4d1f9d512ab63de42cfaf336d8e5e
Erik Versteeg <erik@dexels.com>
2020-08-17 11:42:58 +0200

    Added 2 object functions

--- src/app/utils/functions/validateFunctions.ts
+++ src/app/utils/functions/validateFunctions.ts
@@ -1,5 +1,54 @@
 import { Locale } from '../../types';
 
+export const areEqualObjects = (prevObject: Record<string, unknown>, object: Record<string, unknown>): boolean => {
+    const prevKeys = Object.keys(prevObject);
+    const keys = Object.keys(object);
+    const differKeys = prevKeys.filter((key) => !keys.includes(key));
+
+    if (differKeys.length > 0) {
+        return false;
+    }
+
+    const differValues = prevKeys.filter((key) => {
+        if (typeof prevObject[key] === 'object') {
+            return false;
+        }
+
+        return prevObject[key] !== object[key];
+    });
+
+    if (differValues.length > 0) {
+        return false;
+    }
+
+    return true;
+};
+
+// Returns possible differences between 2 objects
+export const compareObjects = (prevObject: Record<string, unknown>, object: Record<string, unknown>): string[] => {
+    const prevKeys = Object.keys(prevObject);
+    const keys = Object.keys(object);
+    const differKeys = prevKeys.filter((key) => !keys.includes(key));
+
+    if (differKeys.length > 0) {
+        return differKeys;
+    }
+
+    const differValues = prevKeys.filter((key) => {
+        if (typeof prevObject[key] === 'object') {
+            return JSON.stringify(prevObject[key]) !== JSON.stringify(object[key]);
+        }
+
+        return prevObject[key] !== object[key];
+    });
+
+    if (differValues.length > 0) {
+        return differValues;
+    }
+
+    return [];
+};
+
 export const isEmpty = (value: string | unknown | undefined): boolean => {
     if (typeof value === 'string') {
         return value === null || value === '';
