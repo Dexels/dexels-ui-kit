38391db22085e1c17fd3ae732e4d23ae86ae5e88
ayalalevinger <alevinger@dexels.com>
2020-11-23 13:30:41 +0100

    component InformationTable

---
+++ src/app/components/organisms/InformationTable/InformationTable.stories.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent, useMemo } from 'react';
+import InformationTable from './InformationTable';
+import { select } from '@storybook/addon-knobs';
+import tableData from './tableData';
+
+export default { title: 'organisms/InformationTable' };
+
+export const Configurable: FunctionComponent = () => {
+    const data = useMemo(() => tableData(), []);
+
+    return <InformationTable amountOfColumns={select('Columns', [1, 2, 3], 2)} data={data} />;
+};
---
+++ src/app/components/organisms/InformationTable/InformationTable.tsx
@@ -0,0 +1,51 @@
+import { ChildrenWrapper, Column, Label, Row, StyledInformationTable, Value } from './InformationTable.sc';
+import React, { FunctionComponent, ReactNode } from 'react';
+import { AmountOfColumns } from './types';
+
+export interface InformationTableData {
+    isDisabled?: boolean;
+    label: ReactNode;
+    value: ReactNode;
+}
+
+export interface InformationTableProps {
+    amountOfColumns?: AmountOfColumns;
+    children?: ReactNode;
+    data?: InformationTableData[];
+}
+
+const InformationTable: FunctionComponent<InformationTableProps> = ({ amountOfColumns = 2, children, data = [] }) => {
+    const amountOfRowsPerColumn = Math.ceil(data.length / amountOfColumns);
+
+    // eslint-disable-next-line no-console
+    console.log('[amountOfRowsPerColumn]', amountOfRowsPerColumn);
+
+    const columnArray =
+        data.length > 0
+            ? Array.from(Array(amountOfColumns).keys()).map((key) => (
+                  <Column amountOfColumns={amountOfColumns} key={key}>
+                      {data
+                          .slice(key * amountOfRowsPerColumn, (key + 1) * amountOfRowsPerColumn)
+                          .map(({ isDisabled = false, label, value }, index) => (
+                              // eslint-disable-next-line react/no-array-index-key
+                              <Row key={index}>
+                                  <Label>{label}</Label>
+                                  <Value isDisabled={isDisabled}>{value}</Value>
+                              </Row>
+                          ))}
+                  </Column>
+              ))
+            : [];
+
+    // eslint-disable-next-line no-console
+    console.log('[columnArray length]', columnArray.length);
+
+    return (
+        <StyledInformationTable>
+            {columnArray}
+            {children && <ChildrenWrapper>{children}</ChildrenWrapper>}
+        </StyledInformationTable>
+    );
+};
+
+export default InformationTable;
