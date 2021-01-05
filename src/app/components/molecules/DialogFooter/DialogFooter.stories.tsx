001eb8ddea41d1bbb573427486cdf29dc994a581
Veronika Demina <Veronika@Dexelss-MacBook-Pro.local>
2021-01-05 10:25:06 +0100

    add option to show button on the left side to stories

--- src/app/components/molecules/DialogFooter/DialogFooter.stories.tsx
+++ src/app/components/molecules/DialogFooter/DialogFooter.stories.tsx
@@ -1,35 +1,39 @@
 import { Alignment, ButtonSize, ButtonVariant, IconType } from '../../../types';
+import { boolean, text } from '@storybook/addon-knobs';
 import React, { FunctionComponent } from 'react';
 import { action } from '@storybook/addon-actions';
 import DialogFooter from './DialogFooter';
-import { text } from '@storybook/addon-knobs';
 
 export default { title: 'molecules/DialogFooter' };
 
-export const Configurable: FunctionComponent = () => (
-    <DialogFooter
-        buttons={[
-            {
-                alignment: Alignment.LEFT,
-                iconType: IconType.TRASHCAN,
-                onClick: action('On cancel click'),
-                size: ButtonSize.SMALL,
-                variant: ButtonVariant.TEXT_ONLY,
-            },
-            {
-                children: 'Cancel',
-                iconType: IconType.CROSS,
-                onClick: action('On cancel click'),
-                size: ButtonSize.SMALL,
-                variant: ButtonVariant.TEXT_ONLY,
-            },
-            {
-                children: 'Ok',
-                iconType: IconType.CHECK,
-                onClick: action('On confirm click'),
-                size: ButtonSize.SMALL,
-            },
-        ]}
-        text={text('Text', '')}
-    />
-);
+export const Configurable: FunctionComponent = () => {
+    const leftAlignment = boolean('With the button on the left', true);
+
+    return (
+        <DialogFooter
+            buttons={[
+                {
+                    alignment: leftAlignment ? Alignment.LEFT : undefined,
+                    iconType: IconType.TRASHCAN,
+                    onClick: action('On delete click'),
+                    size: ButtonSize.SMALL,
+                    variant: ButtonVariant.TEXT_ONLY,
+                },
+                {
+                    children: 'Cancel',
+                    iconType: IconType.CROSS,
+                    onClick: action('On cancel click'),
+                    size: ButtonSize.SMALL,
+                    variant: ButtonVariant.TEXT_ONLY,
+                },
+                {
+                    children: 'Ok',
+                    iconType: IconType.CHECK,
+                    onClick: action('On confirm click'),
+                    size: ButtonSize.SMALL,
+                },
+            ]}
+            text={text('Text', '')}
+        />
+    );
+};
