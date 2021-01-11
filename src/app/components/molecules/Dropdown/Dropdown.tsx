d547a5e3b8b7362440bc50e1a5c09955446cfefd
Erik Versteeg <erik@dexels.com>
2021-01-11 18:53:45 +0100

    Removed white lines

--- src/app/components/molecules/Dropdown/Dropdown.tsx
+++ src/app/components/molecules/Dropdown/Dropdown.tsx
@@ -68,9 +68,7 @@ export const Dropdown: FunctionComponent<DropdownProps & { [key: string]: any }>
 }) => {
     const [isFocused, setIsFocused] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
-
     const isEmpty = useMemo(() => options && options.length === 0, [options]);
-
     const placeholderText = !isEmpty ? placeholder : noOptionsText || placeholder;
 
     return (
