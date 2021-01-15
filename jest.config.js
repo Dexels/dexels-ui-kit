9be219af05dde1388897210b670f32a962474424
Erik Versteeg <erik@dexels.com>
2021-01-15 15:23:19 +0100

    Fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,24 @@
 module.exports = {
     verbose: true,
-    roots: ["<rootDir>/src"],
+    roots: [
+      '<rootDir>/src',
+    ],
     preset: 'ts-jest',
     testEnvironment: 'node',
-
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-
-    // TypeScript support
-    transform: {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-
-    // DOM cleanup and extended assertions
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     setupFilesAfterEnv: [
-      "@testing-library/react/cleanup-after-each",
-      "@testing-library/jest-dom/extend-expect"
+      '<rootDir>/src/setupEnzyme.ts',
     ],
-
     globals: {
-        "ts-jest": {
-            tsConfigFile: "./tsconfig.json",
-            diagnostics: {
-                warnOnly: true
-            }
-        }
+      'ts-jest': {
+        tsconfig: './tsconfig.json',
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
     },
-
-    // Setup Enzyme
-    snapshotSerializers: ["enzyme-to-json/serializer"],
-    setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"]
-
-  };
+    snapshotSerializers: [
+      'enzyme-to-json/serializer',
+    ],
+    testMatch: null,
+  }
