94de262102ee93a63673315f5ba904f4f33406e1
Erik Versteeg <erik@dexels.com>
2021-01-18 14:02:40 +0100

    Prettified

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,18 @@
 module.exports = {
     verbose: true,
-    roots: [
-      '<rootDir>/src',
-    ],
+    roots: ['<rootDir>/src'],
     preset: 'ts-jest',
     testEnvironment: 'node',
     testRegex: '(/__tests__/|(\\.|/)(test|spec))\\.tsx?$',
-    setupFilesAfterEnv: [
-      '<rootDir>/src/app/__tests__/setupEnzyme.ts',
-    ],
+    setupFilesAfterEnv: ['<rootDir>/src/app/__tests__/setupEnzyme.ts'],
     globals: {
-      'ts-jest': {
-        tsconfig: './tsconfig.json',
-        diagnostics: {
-          warnOnly: true,
+        'ts-jest': {
+            tsconfig: './tsconfig.json',
+            diagnostics: {
+                warnOnly: true,
+            },
         },
-      },
     },
-    snapshotSerializers: [
-      'enzyme-to-json/serializer',
-    ],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
     testMatch: null,
-  }
+};
