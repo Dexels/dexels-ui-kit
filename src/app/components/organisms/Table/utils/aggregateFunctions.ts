3935ff0ab7f56bb72ff5d9bc257f6b3cda6bfa32
Erik Versteeg <erik@dexels.com>
2021-02-12 08:44:25 +0100

    Fixed build issue

--- src/app/components/organisms/Table/utils/aggregateFunctions.ts
+++ src/app/components/organisms/Table/utils/aggregateFunctions.ts
@@ -1,11 +1,14 @@
 import { DEFAULT_LOCALE } from '../../../../../global/constants';
+import { Locale } from '../../../../types';
 import { toMoney } from '../../../../utils/functions/financialFunctions';
 
-export const sum = (values: Array<string | number>, isCurrency = false, locale = DEFAULT_LOCALE): number => {
+// Most unfortunate, but DEFAULT_LOCALE can not be set as deafult value in the params part
+// It causes build failure
+export const sum = (values: Array<string | number>, isCurrency = false, locale?: Locale): number => {
     const total = values.reduce((accumulator: number, currentValue: number | string) => {
         // When it concerns a string, then use toMoney to get a number value
         if (typeof currentValue === 'string' && isCurrency) {
-            return accumulator + toMoney(currentValue, locale).value;
+            return accumulator + toMoney(currentValue, locale || DEFAULT_LOCALE).value;
         }
 
         if (typeof currentValue === 'number') {
