dd41c4788d4b9dca3cde5d4dc9efc68945b59b42
Erik Versteeg <erik@dexels.com>
2021-03-10 10:00:48 +0100

    Export isValidEditableInput

--- src/app/components/organisms/EditableInformation/index.ts
+++ src/app/components/organisms/EditableInformation/index.ts
@@ -1,4 +1,8 @@
 export { EditableInformation, EditableInformationProps } from './EditableInformation';
-export { getValueOfEditableDataComponent, generateDropdownSelectOptionLabel } from './utils/informationDataFunctions';
+export {
+    getValueOfEditableDataComponent,
+    generateDropdownSelectOptionLabel,
+    isValidEditableInput,
+} from './utils/informationDataFunctions';
 export { generateValuesArray } from './utils/generateValuesArray';
 export { EditableInformationDataType, ValueTypes, EditableInformationData } from './types';
