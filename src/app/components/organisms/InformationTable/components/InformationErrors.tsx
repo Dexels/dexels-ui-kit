7089292b5ea19326d7ddd46fe68340f5d3ecc424
Erik Versteeg <erik@dexels.com>
2021-03-24 10:55:36 +0100

    Fixed babel errors

--- src/app/components/organisms/InformationTable/components/InformationErrors.tsx
+++ src/app/components/organisms/InformationTable/components/InformationErrors.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 
 export const InformationErrors = (errors: string[]): ReactNode => (
     <ErrorMessageWrapper>
+        {/* eslint-disable-next-line react/destructuring-assignment */}
         {errors?.map((error, index) => (
             // eslint-disable-next-line react/no-array-index-key
             <ErrorMessage key={index}>{error}</ErrorMessage>
