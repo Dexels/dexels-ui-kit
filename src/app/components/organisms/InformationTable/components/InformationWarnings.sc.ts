d13c9e3fc36529c1933e99386216b7a9a69974c1
Erik Versteeg <erik@dexels.com>
2021-04-06 17:18:24 +0200

    Changed warning color

--- src/app/components/organisms/InformationTable/components/InformationWarnings.sc.ts
+++ src/app/components/organisms/InformationTable/components/InformationWarnings.sc.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { themeBasic } from '../../../../styles/theming/themes/basic';
 
 export const WarningMessage = styled(Message)`
-    color: ${({ theme }): string => theme.colorText.primary};
+    color: ${({ theme }): string => theme.colorSecondary};
 `;
 
 WarningMessage.defaultProps = {
@@ -11,7 +11,7 @@ WarningMessage.defaultProps = {
 };
 
 export const WarningMessageWrapper = styled(MessageWrapper)`
-    border-left: ${({ theme }): string => `8px solid ${theme.colorText.primary}`};
+    border-left: ${({ theme }): string => `8px solid ${theme.colorSecondary}`};
 `;
 
 WarningMessageWrapper.defaultProps = {
