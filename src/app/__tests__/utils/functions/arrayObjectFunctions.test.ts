3f30e8383044f3675efeab339e6e0d7610163e2e
Erik Versteeg <erik@dexels.com>
2021-04-09 17:56:38 +0200

    Added some tests

---
+++ src/app/__tests__/utils/functions/arrayObjectFunctions.test.ts
@@ -0,0 +1,16 @@
+import { getDropdownSelectOption, selectOptionsFacade } from '../../../utils/functions/arrayObjectFunctions';
+import { dropdownSelectOptions } from '../../../components/organisms/DropdownSelect/mockup/data';
+import { fruitOptions } from '../../../components/molecules/Dropdown/mockup/data';
+
+describe('test arrayObject functions', () => {
+    test('test getDropdownSelectOption', () => {
+        expect(getDropdownSelectOption(dropdownSelectOptions, 'APPLES')).toBeUndefined();
+        expect(getDropdownSelectOption(dropdownSelectOptions, 'APPLE')).toBeDefined();
+        expect(getDropdownSelectOption(dropdownSelectOptions, 'ORANGE')?.label).toEqual('Orange');
+    });
+
+    test('test selectOptionsFacade', () => {
+        expect(selectOptionsFacade(fruitOptions, 'Name', 'Id')).toBeDefined();
+        expect(selectOptionsFacade(fruitOptions, 'Name', 'Id')).toHaveLength(4);
+    });
+});
