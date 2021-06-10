f0a5b6d05adb2a94dadf4d6080c19693acac0f1c
ayalalevinger <alevinger@dexels.com>
2021-06-10 13:29:35 +0200

    removed forwardRef, header checkbox works without it

--- src/app/components/organisms/Table/RowSelectionCheckbox/RowSelectionCheckbox.tsx
+++ src/app/components/organisms/Table/RowSelectionCheckbox/RowSelectionCheckbox.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { ChangeEvent, FunctionComponent, MutableRefObject } from 'react';
+import React, { ChangeEvent, FunctionComponent } from 'react';
 import SelectionControl from '../../../molecules/SelectionControl/SelectionControl';
 import { SelectionControlSize } from '../../../../types';
 import { SelectionControlType } from '../../../molecules/SelectionControl/types';
@@ -12,40 +12,29 @@ export interface RowSelectionCheckboxProps {
     selectedProps: Partial<TableToggleRowsSelectedProps>;
 }
 
-export const RowSelectionCheckbox: FunctionComponent<RowSelectionCheckboxProps> = React.forwardRef(
-    ({ isDisabled = false, isHeader = false, selectedProps }, ref) => {
-        const defaultRef = React.useRef<any>();
-        const resolvedRef = (ref as MutableRefObject<any>) || defaultRef;
-
-        React.useEffect(() => {
-            if (resolvedRef.current) {
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                resolvedRef.current.indeterminate = selectedProps.indeterminate;
-            }
-        }, [resolvedRef, selectedProps]);
-
-        return (
-            <Wrapper isHeader={isHeader}>
-                <SelectionControl
-                    hasHorizontalCorrection={false}
-                    hasVerticalCorrection={false}
-                    isChecked={selectedProps.checked}
-                    isDisabled={isDisabled}
-                    isIndeterminate={selectedProps.indeterminate}
-                    isTableElement
-                    label=""
-                    name={isHeader ? '_selector_header' : '_selector_row'} // @TODO: figure out if the non unique name will cause issues
-                    onChange={(event): void => {
-                        if (selectedProps.onChange !== undefined) {
-                            selectedProps.onChange(event as ChangeEvent<HTMLInputElement>);
-                        }
-                    }}
-                    ref={resolvedRef}
-                    size={SelectionControlSize.SMALL}
-                    type={SelectionControlType.CHECKBOX}
-                    value=""
-                />
-            </Wrapper>
-        );
-    }
+export const RowSelectionCheckbox: FunctionComponent<RowSelectionCheckboxProps> = ({
+    isDisabled = false,
+    isHeader = false,
+    selectedProps,
+}) => (
+    <Wrapper isHeader={isHeader}>
+        <SelectionControl
+            hasHorizontalCorrection={false}
+            hasVerticalCorrection={false}
+            isChecked={selectedProps.checked}
+            isDisabled={isDisabled}
+            isIndeterminate={selectedProps.indeterminate}
+            isTableElement
+            label=""
+            name={isHeader ? '_selector_header' : '_selector_row'} // @TODO: figure out if the non unique name will cause issues
+            onChange={(event): void => {
+                if (selectedProps.onChange !== undefined) {
+                    selectedProps.onChange(event as ChangeEvent<HTMLInputElement>);
+                }
+            }}
+            size={SelectionControlSize.SMALL}
+            type={SelectionControlType.CHECKBOX}
+            value=""
+        />
+    </Wrapper>
 );
